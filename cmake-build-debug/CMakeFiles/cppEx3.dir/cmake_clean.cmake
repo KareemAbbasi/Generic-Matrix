9e4c654d10d0d4c1cffe1e8f3a26a2c13a9bdb2e
Kareem Abbasi <kareem.abbasi@mail.huji.ac.il>
2019-01-08 11:04:25 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/cppEx3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppEx3.dir/main.cpp.o"
+  "cppEx3.pdb"
+  "cppEx3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppEx3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
