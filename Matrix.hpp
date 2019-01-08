d5329b895f6951a934495822b5915fac7a9b0a83
Kareem Abbasi <kareem.abbasi@mail.huji.ac.il>
2019-01-08 17:10:43 +0200

    Implemented the constructors.

--- Matrix.hpp
+++ Matrix.hpp
@@ -7,8 +7,16 @@ class Matrix
 public:
     Matrix();
     Matrix(unsigned int rows, unsigned int cols);
+    Matrix(const Matrix& mat);
+    Matrix(unsigned int rows, unsigned int cols, const std::vector<T> & cells);
+    //TODO Destructor.
 
-private:
+    
+
+    void printMat();
+
+//TODO make private
+//private:
     unsigned int _numColumns, _numRows;
     std::vector<std::vector <T>> _matrix;
 
@@ -21,7 +29,7 @@ Matrix<T>::Matrix()
     std::vector<T> row;
     row.resize(1);
     _matrix.push_back(row);
-//    _matrix.resize(1);
+
     //TODO remove this shit.
     std::cout << _matrix.size() << _matrix.at(0).size() <<  std::endl;
     std::cout << _matrix.at(0).at(0) << std::endl;
@@ -39,6 +47,7 @@ Matrix<T>::Matrix(unsigned int rows, unsigned int cols)
         _matrix[i].resize(_numColumns);
     }
 
+    //TODO remove
     for (unsigned i = 0; i < _matrix.size(); ++i)
     {
         for (unsigned j = 0 ; j < _numColumns; ++j)
@@ -47,5 +56,54 @@ Matrix<T>::Matrix(unsigned int rows, unsigned int cols)
         }
         std::cout << "" << std::endl;
     }
+}
 
+template <class T>
+Matrix<T>::Matrix(const Matrix &mat)
+{
+    _numColumns = mat._numColumns;
+    _numRows = mat._numRows;
+    _matrix.resize(_numRows);
+
+    for (unsigned i = 0; i < _matrix.size(); ++i)
+    {
+        _matrix[i].resize(_numColumns);
+        for (unsigned j = 0 ; j < _numColumns; ++j)
+        {
+            _matrix[i][j] = mat._matrix[i][j];
+        }
+    }
+}
+
+template <class T>
+Matrix<T>::Matrix(unsigned int rows, unsigned int cols, const std::vector<T> &cells)
+{
+    _numColumns = cols;
+    _numRows = rows;
+    _matrix.resize(_numRows);
+
+    for (unsigned i = 0; i < _matrix.size(); ++i)
+    {
+        _matrix[i].resize(_numColumns);
+        for (unsigned j = 0; j < _numColumns; ++j)
+        {
+            //TODO check if cells doesn't have values in the current index, maybe using exceptions.
+            _matrix[i][j] = cells[j + i * _numColumns];
+        }
+    }
+}
+
+
+
+template <class T>
+void Matrix<T>::printMat()
+{
+    for (unsigned i = 0; i < _matrix.size(); ++i)
+    {
+        for (unsigned j = 0 ; j < _numColumns; ++j)
+        {
+            std::cout << _matrix[i][j] << " ";
+        }
+        std::cout << "" << std::endl;
+    }
 }
--- main.cpp
+++ main.cpp
@@ -3,7 +3,25 @@
 #include "Complex.h"
 
 int main() {
-    Matrix<Complex> nM(3,6);
+    Matrix<int> nM(3, 6);
+
+    std::cout << "1 done" << std::endl;
+
+    Matrix<int> ns(nM);
+    std::cout << "2 done" << std::endl;
+    nM._matrix.at(1).at(2) = 3;
+    nM.printMat();
+    std::cout << "" << std::endl;
+    ns.printMat();
+
+    std::cout << "" << std::endl;
+    std::cout << "NEW MATRIX FROM VECTOR" << std::endl;
+    std::cout << "" << std::endl;
+
+    std::vector<int> v = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+    Matrix<int> matVect(3, 5, v);
+    matVect.printMat();
 
     std::cout << "Hello, World!" << std::endl;
     return 0;
